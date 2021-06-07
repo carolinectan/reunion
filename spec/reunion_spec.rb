70dff7a569e1c2f5b4b98ef4e06fc8cc604fca84
Caroline Tan <carolinectan3@gmail.com>
2021-06-07 10:48:03 -0400

    Add tests for reunion

--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -3,7 +3,7 @@ require './lib/reunion'
 
 RSpec.describe Reunion do
   before :each do
-    @reunion = Reunion.new("Brunch")
+    @reunion = Reunion.new("1406 BE")
   end
 
   it 'exists' do
@@ -21,5 +21,35 @@ RSpec.describe Reunion do
     @reunion.add_activity(@activity_1)
 
     expect(@reunion.activities).to eq([@activity_1])
+  end
+
+  it 'can breakout what each person owes for the reunions' do
+    @activity_1 = Activity.new("Brunch")
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+    @reunion.add_activity(activity_1)
+
+    expect(@reunion.total_cost).to eq(60)
+
+    @activity_2 = Activity.new("Drinks")
+    @activity_2.add_participant("Maria", 60)
+    @activity_2.add_participant("Luther", 60)
+    @activity_2.add_participant("Louis", 0)
+    @reunion.add_activity(@activity_2)
+
+    expect(@reunion.total_cost).to eq(180)
+
+    expected = {
+                  "Maria" => -10,
+                  "Luther" => -30,
+                  "Louis" => 40
+                }
+    expect(@reunion.breakout).to eq(expected)
+
+    expect(@reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
+
+    expect(puts @reunion.summary).to eq("Maria: -10
+      Luther: -30
+      Louis: 40")
   end
 end
