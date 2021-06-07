8ad53cb029f0d62555347954d582278fa5ea9fda
Caroline Tan <carolinectan3@gmail.com>
2021-06-07 09:51:45 -0400

    Create activity class and methods

---
+++ lib/activity.rb
@@ -0,0 +1,15 @@
+class Activity
+  attr_reader :name, :participants, :total_cost
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+    @total_cost = 0
+  end
+
+  def add_participant(name, payment)
+    @participants[name] = payment
+
+    @total_cost = @total_cost + payment
+  end
+end
